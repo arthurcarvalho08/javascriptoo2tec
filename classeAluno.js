ec00ede776d12733e8ddf20210d1881c7634e3a7
arthurcarvalho08 <arthur.galego.carvalho@escola.pr.gov.br>
2026-04-13 17:23:52 +0000

    aprovação escolar

--- classeAluno.js
+++ classeAluno.js
@@ -11,7 +11,22 @@ class Aluno {
         }
         return soma / this.notas.length;
     }
+
+    obterStatus(){
+        const media = this.calcular_media()
+
+        if (media >= 7){
+            return "Aprovado"
+        }
+        else if (media >= 5){
+            return "Recuperação"
+        }
+        else{
+            return "Reprovado"
+        }
+    }
 }
 
 let aluno1 = new Aluno("Meio Kilo", 55050, [8.0, 5.7, 9.9]);
 console.log(aluno1.calcular_media());
+console.log(aluno1.obterStatus());
